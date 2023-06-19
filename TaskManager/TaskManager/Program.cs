71208364f7b86093045c5dde5aed8f72e06fd3ca
PeggyBrown <ola.qnysz@gmail.com>
2023-06-19 15:13:16 +0200

    Added tests project

--- TaskManager/TaskManager/Program.cs
+++ TaskManager/TaskManager/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TaskManager
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is TaskManager!");
+        }
+    }
+}
